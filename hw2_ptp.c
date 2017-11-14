3327bb638919bb9a01d40de8cb458f97d5b678fb
Dinggao Pan <dinggao_pan@hotmail.com>
2017-11-13 20:09:52 -0800

    ptp event read ready
    need polishing and get rid of first jump

--- hw2_ptp.c
+++ hw2_ptp.c
@@ -4,41 +4,58 @@
 #include <fcntl.h>
 #include <time.h>
 #include <string.h>
+#include <sys/ioctl.h>
 
 #include <linux/ptp_clock.h>
 
 #define PTPCHN1 "/dev/ptp1"
-#define TSFLAGS PTP_ENABLE_FEATURE | PTP_RISING_EDGE;
+#define CONFIGPIN "\
+#/bin/bash \n\
+config-pin overlay cape-universala\n\
+config-pin P8_10 timer\n\
+"
 
-int fd;
-struct ptp_extts_event event;
+int fd, cnt, start;
+struct ptp_clock_caps caps;
+struct ptp_extts_event extts_event;
+struct ptp_extts_event init_extts_event;
 struct ptp_extts_request extts_request;
 
 int main(int argc, char *charv[])
 {
+  system(CONFIGPIN);
+  
   fd = open(PTPCHN1, O_RDWR);
   if(fd < 0)
   {
-    fprintf(stderr, "ptp device not correctly loaded");
-    return -1;
+    perror("ptp1");
   }
+  start = 1;
+  memset(&extts_request, 0, sizeof(extts_request));
+  extts_request.index = 1;
+  extts_request.flags = PTP_ENABLE_FEATURE | PTP_RISING_EDGE;
   
-    memset(&extts_request, 0, sizeof(extts_request));
-    extts_request.index = 0;
-    extts_request.flags = TSFLAGS;
-    if (ioctl(fd, PTP_EXTTS_REQUEST, &extts_request)) {
-      fprintf(stderr, "PTP_EXTTS_REQUEST failed");
-    } else {
-      fprintf(stdout, "Request successful!");
-    }
-    
-    // Closing the ptp1 channel
-    extts_request.flags = 0;
-    ioctl(fd, PTP_EXTTS_REQUEST, &extts_request);
-    
-    //read(fd, &event);
-    
+  if (ioctl(fd, PTP_EXTTS_REQUEST, &extts_request)) {
+    perror("PTP_EXTTS_REQUEST");
+  } else {
+    fprintf(stdout, "Request successful!\n");
+  }
+  int i;
+  for(i = 0; i < 20; i++) {
+  if (start) {
+    read(fd, &init_extts_event, sizeof(init_extts_event));
+    start = 0;
+  }
+	read(fd, &extts_event, sizeof(extts_event));
+  fprintf(stdout, "Event Channel:%d, Event timestamp: %lld.%09u\n", extts_event.index, 
+          extts_event.t.sec - init_extts_event.t.sec, extts_event.t.nsec - init_extts_event.t.nsec);
+  
+  }
+  // Closing the ptp1 channel
+  extts_request.flags = 0;
+  ioctl(fd, PTP_EXTTS_REQUEST, &extts_request);
   close(fd);
-  fprintf(stdout, "I am done.");
+
+  fprintf(stdout, "I am done.\n");
   return 0;
 }

585e356ac7216b591fffb47a4fe9ca55064a5ac5
Dinggao Pan <dinggao_pan@hotmail.com>
2017-11-13 17:53:34 -0800

    test configuration of ptp channel

--- hw2_ptp.c
+++ hw2_ptp.c
@@ -6,9 +6,12 @@
 
 #include <linux/ptp_clock.h>
 
-#define PTPCHN1 "/dev/ptp0"
+#define PTPCHN1 "/dev/ptp1"
+#define TSFLAGS PTP_ENABLE_FEATURE | PTP_RISING_EDGE;
 
 int fd;
+struct ptp_extts_event event;
+struct ptp_extts_request extts_request;
 
 int main(int argc, char *charv[])
 {
@@ -19,7 +22,22 @@ int main(int argc, char *charv[])
     return -1;
   }
   
-  
-  
+    memset(&extts_request, 0, sizeof(extts_request));
+    extts_request.index = 0;
+    extts_request.flags = TSFLAGS;
+    if (ioctl(fd, PTP_EXTTS_REQUEST, &extts_request)) {
+      fprintf(stderr, "PTP_EXTTS_REQUEST failed");
+    } else {
+      fprintf(stdout, "Request successful!");
+    }
+    
+    // Closing the ptp1 channel
+    extts_request.flags = 0;
+    ioctl(fd, PTP_EXTTS_REQUEST, &extts_request);
+    
+    //read(fd, &event);
+    
+  close(fd);
+  fprintf(stdout, "I am done.");
   return 0;
 }

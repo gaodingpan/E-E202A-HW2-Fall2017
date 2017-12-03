5e616e6fd840c31319e7cd0795258cbcdbf18547
Ron Ocampo <user@example.com>
2017-12-02 22:23:53 -0800

    Revert "clean up"
    This reverts commit 5a804e7d1f803779d89b27b7dcb050a8327fb427.

--- pru_pulseGen/pru_main.c
+++ pru_pulseGen/pru_main.c
@@ -25,7 +25,6 @@
 #define SHARED_MEM_BASE 0x10000
 
 uint64_t curTime, prevTime;
-uint64_t offset_ns = 8000;
 int first = 1;
 
 void
@@ -42,6 +41,12 @@ int main()
 	// Wait enough time for host to startup
     WAIT_MS(1000);
 
+    /*
+    // Send message from PRU to host
+    struct rbuffer *send_buf = (struct rbuffer *) (uint32_t) (SHARED_MEM_BASE);
+    init_rbuffer(send_buf);
+	*/
+
 	//	Receive timestamp from host to PRU
 	struct rbuffer *rec_buf = (struct rbuffer *) (uint32_t) (SHARED_MEM_BASE);
 
@@ -52,6 +57,7 @@ int main()
     init_iep_time(&time);
 
 	uint32_t waitTime = 0;
+	//int curCycle = 0;
 
 	short status = -1;
 
@@ -63,7 +69,6 @@ int main()
 		}
 
 		waitTime = rbuf_read_uint64(rec_buf, &status);
-
 		//if you received a new waitTime
 		if(waitTime != 0) {
 			//toggle pin for 50us
@@ -78,8 +83,7 @@ int main()
 			}			
 
 			//spin until it is time for the next pulse
-			//offset for autograder error correction
-			while((curTime - prevTime) < (waitTime + offset_ns)) {
+			while((curTime - prevTime) < waitTime) {
 				curTime = iep_get_time(&time);
 			}
 
@@ -91,7 +95,28 @@ int main()
 			prevTime = curTime;
 			TRIG_INTC(3); // Trigger interrupt PRUEVENT_0
 
-			//ready for new waitTime
+			//ready for new wait time
 		}
+
+    	/*
+        waitTime = rbuf_read_uint32(rec_buf, &status);
+		if(waitTime < 0)// Oringinally for breaking out of the loop, should use the com in new example you used
+		{
+			break;
+		}
+		while(curCycle < waitTime / 2) {
+			curCycle += 3;//while loop is around 2 cycles, the addition counts for 1
+		}
+		//WAIT_CYCLES((int)waitTime);
+        TRIG_INTC(3); // Trigger interrupt PRUEVENT_0 for update of next waitTime
+		while(curCycle < waitTime) {
+			curCycle += 3;//while loop is around 2 cycles, the addition counts for 1
+		}
+        //WAIT_CYCLES((int)waitTime - 3);
+		toggle_pin(P8_46);
+		WAIT_CYCLES(98);
+		toggle_pin(P8_46);//combined 100ms pulse
+		curCycle = 0;
+		*/
     }
 }

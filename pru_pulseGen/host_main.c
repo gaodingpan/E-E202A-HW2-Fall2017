54b4e2158973d8904d566e545b7126730c0e46c5
Dinggao Pan <dinggao_pan@hotmail.com>
2017-12-02 22:50:02 -0800

    Revert "clean up"
    This reverts commit 5a804e7d1f803779d89b27b7dcb050a8327fb427.

--- pru_pulseGen/host_main.c
+++ pru_pulseGen/host_main.c
@@ -24,8 +24,18 @@
 
 #define ERROR 0 //static error for passing the variable to pru;
 
+#define ERROR 0 //static error for passing the variable to pru;
+
 volatile int stop = 0;
 volatile uint8_t *shared_mem;
+<<<<<<< HEAD
+=======
+struct rbuffer *terminate_pru_rbuffer;
+struct rbuffer *send_to_pru_rbuffer;
+//struct rbuffer *receive_from_pru_rbuffer;
+
+int first = 1;
+>>>>>>> parent of 5a804e7... clean up
 double curTimeStamp, prevTimeStamp;
 uint32_t waitTime;
 FILE *inputPtr;
@@ -35,8 +45,85 @@ uint32_t calculateWait(double curTimeStamp, double prevTimeStamp)
 	return (((curTimeStamp - prevTimeStamp) * 200000000 - 20000000 - ERROR) / 2);
 }
 
+<<<<<<< HEAD
 void *threadFunction(void *value){
     do {
+=======
+void nomoreinputs() {
+	printf("\nno more inputs \n");
+	stop = 1;
+	
+	//close PRU
+	//buffer value 1 means terminate
+    rbuf_write_uint64(terminate_pru_rbuffer, 1);
+	printf("pru terminate\n");
+	
+	prussdrv_pru_disable(PRU_NUM);
+    prussdrv_exit ();
+
+	exit(0);
+}
+
+/*
+uint32_t calculateWait(double curTimeStamp, double prevTimeStamp)
+{
+	return (((curTimeStamp - prevTimeStamp) * 200000000 - 20000000 - ERROR) / 2);
+}
+*/
+
+void *receive_pru_thread(void *value){
+/*
+    do {
+        prussdrv_pru_wait_event(PRU_EVTOUT_0);
+        prussdrv_pru_clear_event(PRU_EVTOUT_0, PRU0_ARM_INTERRUPT);
+        struct rbuffer *rbuf = (struct rbuffer *) shared_mem;
+		while((curTimeStamp - prevTimeStamp < 100)){ 
+			//fscanf();
+		} 
+		//waitTime = calculateWait(curTimeStamp, prevTimeStamp);
+		rbuf_write_uint64(send_to_pru_rbuffer, waitTime);
+		prevTimeStamp = curTimeStamp;
+		//fscanf();
+    } while (curTimeStamp != prevTimeStamp);
+	prussdrv_pru_wait_event(PRU_EVTOUT_0);
+    prussdrv_pru_clear_event(PRU_EVTOUT_0, PRU0_ARM_INTERRUPT);
+    struct rbuffer *rbuf = (struct rbuffer *) shared_mem;
+	rbuf_write_uint64(send_to_pru_rbuffer, -1);
+*/	
+}
+
+// Handles sending to PRU
+void *send_pru_thread(void *value)
+{
+	printf("started send thread\n");
+    int err = 0;
+    //uint64_t nano_ts;
+    //int i = 1;
+
+    while (!stop) {
+
+    	//scan input and get current time
+    	if (EOF == fscanf(inputPtr, "%lf", &curTimeStamp))
+    	{
+    		//no more inputs quit
+    		nomoreinputs();
+    	}
+
+    	//printf("file scanned %lf\n", curTimeStamp);
+
+    	if (first)
+    	{
+    		first = 0;
+    		prevTimeStamp = curTimeStamp;
+    		//scan again
+    		fscanf(inputPtr, "%lf", &curTimeStamp);
+    	}
+
+    	waitTime = (curTimeStamp - prevTimeStamp)*1000000000;
+    	rbuf_write_uint64(send_to_pru_rbuffer, waitTime);
+    	// This call will block until PRU interrupts the host
+    	//waits until PRU has read buffer before putting a new value
+>>>>>>> parent of 5a804e7... clean up
         prussdrv_pru_wait_event(PRU_EVTOUT_0);
         prussdrv_pru_clear_event(PRU_EVTOUT_0, PRU0_ARM_INTERRUPT);
         struct rbuffer *rbuf = (struct rbuffer *) shared_mem;
@@ -78,6 +165,7 @@ int main (int argc, char *argv[])
 	}
 	
     pthread_t thread;
+    pthread_t thread2;
 
     /* Initialize structure used by prussdrv_pruintc_intc   */
     /* PRUSS_INTC_INITDATA is found in pruss_intc_mapping.h */
@@ -96,6 +184,7 @@ int main (int argc, char *argv[])
         exit(EXIT_FAILURE);
     }
 
+<<<<<<< HEAD
 	/* Map PRU's INTC */
 	prussdrv_pruintc_int(&pruss_intc_initdata);
 	
@@ -108,6 +197,31 @@ int main (int argc, char *argv[])
 	
 	memset((void*) shared_mem, 0, 0x2000);
 	
+=======
+    /* Map PRU's INTC */
+    prussdrv_pruintc_init(&pruss_intc_initdata);
+
+    // Map PRU's shared memory into user-space
+    if (prussdrv_map_prumem(PRUSS0_SHARED_DATARAM, (void **) &shared_mem)) {
+        printf("map shared memory failed\n");
+        exit(EXIT_FAILURE);
+    }
+
+    // Clear 12Kb of shared memory
+    memset((void*) shared_mem, 0, 0x3000);
+
+    // Setup buffers for communication between host and PRU
+
+    // Only the sender initializes the rbuffer.
+    // PRU will initialize this buffer. Host should NOT initalize this.
+    //receive_from_pru_rbuffer = (struct rbuffer *) (shared_mem);
+
+    send_to_pru_rbuffer = (struct rbuffer *) (shared_mem);
+    init_rbuffer(send_to_pru_rbuffer);
+
+    terminate_pru_rbuffer = (struct rbuffer *) (shared_mem + sizeof(struct rbuffer));
+    init_rbuffer(terminate_pru_rbuffer);
+>>>>>>> parent of 5a804e7... clean up
 
     /* Load the memory data file */
     prussdrv_load_datafile(PRU_NUM, "./data.bin");
@@ -115,8 +229,19 @@ int main (int argc, char *argv[])
     /* Load and execute binary on PRU */
     prussdrv_exec_program (PRU_NUM, "./text.bin");
 
+<<<<<<< HEAD
     /* Wait for event completion from PRU */
     if (pthread_create(&thread, NULL, &threadFunction, NULL)){
+=======
+    /* Thread for handling message from the PRU */
+    if (pthread_create(&thread, NULL, &receive_pru_thread, NULL)){
+        printf("Failed to create thread!\n");
+        exit(EXIT_FAILURE);
+    }
+
+    /* Thread for handling messages sent to the PRU */
+    if (pthread_create(&thread2, NULL, &send_pru_thread, NULL)){
+>>>>>>> parent of 5a804e7... clean up
         printf("Failed to create thread!\n");
         exit(EXIT_FAILURE);
     }
--- pru_pulseGen/pru_main.c
+++ pru_pulseGen/pru_main.c
@@ -19,6 +19,7 @@
 #include "nesl_pru_rbuffer.h"
 #include "nesl_pru_gpio.h"
 #include <stdint.h>
+<<<<<<< HEAD
 
 int main()
 {
@@ -26,6 +27,48 @@ int main()
     init_rbuffer(rbuf);
 	uint32_t waitTime = 0;
 	int curCycle = 0;
+=======
+#include <stdlib.h>
+
+#define SHARED_MEM_BASE 0x10000
+
+uint64_t curTime, prevTime;
+int first = 1;
+
+void
+terminate()
+{
+	deassert_pin(P8_46);
+    TRIG_INTC(4); // Trigger interrupt PRUEVENT_1
+    __halt(); // halt the PRU
+}
+
+int main()
+{
+
+	// Wait enough time for host to startup
+    WAIT_MS(1000);
+
+    /*
+    // Send message from PRU to host
+    struct rbuffer *send_buf = (struct rbuffer *) (uint32_t) (SHARED_MEM_BASE);
+    init_rbuffer(send_buf);
+	*/
+
+	//	Receive timestamp from host to PRU
+	struct rbuffer *rec_buf = (struct rbuffer *) (uint32_t) (SHARED_MEM_BASE);
+
+    //  Receive buffer from host to PRU
+    struct rbuffer *terminate_buf = (struct rbuffer *) (uint32_t) (SHARED_MEM_BASE + sizeof(struct rbuffer));
+
+    struct iep_time time;
+    init_iep_time(&time);
+
+	uint32_t waitTime = 0;
+	//int curCycle = 0;
+
+	short status = -1;
+>>>>>>> parent of 5a804e7... clean up
 
     while(1) {
         waitTime = rbuf_read_uint32(rbuf, 0);
@@ -48,14 +91,56 @@ int main()
 		curCycle = 0;
     }
 
+<<<<<<< HEAD
 
     // Exiting the application - send the interrupt
     TRIG_INTC(4); // Trigger interrupt PRUEVENT_1
     __halt(); // halt the PRU
 }
 
+=======
+		waitTime = rbuf_read_uint64(rec_buf, &status);
+		//if you received a new waitTime
+		if(waitTime != 0) {
+			//toggle pin for 50us
+>>>>>>> parent of 5a804e7... clean up
+
+
+<<<<<<< HEAD
+=======
+			//spin until it is time for the next pulse
+			while((curTime - prevTime) < waitTime) {
+				curTime = iep_get_time(&time);
+			}
+>>>>>>> parent of 5a804e7... clean up
 
 
 
+<<<<<<< HEAD
+=======
+			//ready for new wait time
+		}
 
-
+    	/*
+        waitTime = rbuf_read_uint32(rec_buf, &status);
+		if(waitTime < 0)// Oringinally for breaking out of the loop, should use the com in new example you used
+		{
+			break;
+		}
+		while(curCycle < waitTime / 2) {
+			curCycle += 3;//while loop is around 2 cycles, the addition counts for 1
+		}
+		//WAIT_CYCLES((int)waitTime);
+        TRIG_INTC(3); // Trigger interrupt PRUEVENT_0 for update of next waitTime
+		while(curCycle < waitTime) {
+			curCycle += 3;//while loop is around 2 cycles, the addition counts for 1
+		}
+        //WAIT_CYCLES((int)waitTime - 3);
+		toggle_pin(P8_46);
+		WAIT_CYCLES(98);
+		toggle_pin(P8_46);//combined 100ms pulse
+		curCycle = 0;
+		*/
+    }
+}
+>>>>>>> parent of 5a804e7... clean up
